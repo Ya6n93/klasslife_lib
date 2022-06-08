cb651521d11f60d000202170d693795ba306b8be
Dekukou <puig_j@etna-alternance.net>
2022-06-08 16:34:24 +0200

    Update package.json

--- src/api/auth/login.js
+++ src/api/auth/login.js
@@ -1,12 +1,8 @@
 import axios from 'axios'
 
 const Login = async (url, data) => {
-  try {
-    const response = axios.post(`${url}/login`, data)
-    return response.data
-  } catch (error) {
-    return { error: error.response.data.message, status: error.response.status }
-  }
+  const response = axios.post(`${url}/login`, data)
+  return response
 }
 
 export { Login }
