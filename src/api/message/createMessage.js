3441fcaa6ad632449fb2bbfcbdcce80b3ab3637a
latrac_y <latrac_y@etna-alternance.net>
2022-06-29 15:31:41 +0200

    add id to createMessage

--- src/api/message/createMessage.js
+++ src/api/message/createMessage.js
@@ -1,7 +1,7 @@
 import axios from '../../lib/axios'
 
-const createMessage = async (url, token, data) => {
-  const response = await axios.post(`${url}/messages`, data, {
+const createMessage = async (url, token, id, data) => {
+  const response = await axios.post(`${url}/messages/${id}`, data, {
     headers: { Authorization: `Bearer ${token}` }
   })
 
