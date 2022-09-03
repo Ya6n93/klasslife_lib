eda666f2c0813d7a42685b3200777bce286a2c35
Jérémy Puig <puig_j@etna-alternance.net>
2022-09-03 10:24:05 +0200

    update getActivities route

--- src/api/activities/getActivities.js
+++ src/api/activities/getActivities.js
@@ -1,7 +1,13 @@
 import axios from '../../lib/axios'
 
-const getActivities = async (url, token) => {
-  const response = await axios.get(`${url}/activities`, {
+const getActivities = async (url, token, params) => {
+  let paramsString = ''
+  if (params) {
+    paramsString = `?${Object.keys(params)
+      .map((key) => `${key}=${params[key]}`)
+      .join('&')}`
+  }
+  const response = await axios.get(`${url}/activities${paramsString}`, {
     headers: { Authorization: `Bearer ${token}` }
   })
 
