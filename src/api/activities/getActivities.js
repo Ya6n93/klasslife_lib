889fa476d6c36aa616a49069ea0feab6c0e4697d
Jérémy Puig <puig_j@etna-alternance.net>
2022-09-29 15:29:04 +0200

    Mise à jour de params dans 2 routes

--- src/api/activities/getActivities.js
+++ src/api/activities/getActivities.js
@@ -1,15 +1,13 @@
 import axios from '../../lib/axios'
 
-const getActivities = async (url, token, params) => {
+const getActivities = async (url, params) => {
   let paramsString = ''
   if (params) {
     paramsString = `?${Object.keys(params)
       .map((key) => `${key}=${params[key]}`)
       .join('&')}`
   }
-  const response = await axios.get(`${url}/activities${paramsString}`, {
-    headers: { Authorization: `Bearer ${token}` }
-  })
+  const response = await axios.get(`${url}/activities${paramsString}`)
 
   return response
 }
--- src/api/classrooms/getClassrooms.js
+++ src/api/classrooms/getClassrooms.js
@@ -1,7 +1,14 @@
 import axios from '../../lib/axios'
 
-const getClassrooms = async (url, token) => {
-  const response = await axios.get(`${url}/classrooms`, {
+const getClassrooms = async (url, token, params) => {
+  let paramsString = ''
+  if (params) {
+    paramsString = `?${Object.keys(params)
+      .map((key) => `${key}=${params[key]}`)
+      .join('&')}`
+  }
+
+  const response = await axios.get(`${url}/classrooms${paramsString}`, {
     headers: { Authorization: `Bearer ${token}` }
   })
 
