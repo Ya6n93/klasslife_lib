35b3bcd46e623b7fd68c07753589c5781ac2c92a
Jérémy Puig <puig_j@etna-alternance.net>
2022-10-02 10:22:32 +0200

    Nouvelle route update Student Activities

--- src/api/studentActivities/updateStudentActivity.js
+++ src/api/studentActivities/updateStudentActivity.js
@@ -2,7 +2,7 @@ import axios from '../../lib/axios'
 
 const updateStudentActivity = async (url, studentActivityId, data) => {
   const response = await axios.put(
-    `${url}/student_activities/${studentActivityId}`,
+    `${url}/update/student_activities/${studentActivityId}`,
     data
   )
 
